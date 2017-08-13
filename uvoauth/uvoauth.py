e50c91e0a6050ecb58fc765335d466e2f927cbc1
Justin Barrick <justin.m.barrick@gmail.com>
2017-08-13 18:51:40 +0000

    Fix trailing whitespace issue.

--- uvoauth/uvoauth.py
+++ uvoauth/uvoauth.py
@@ -108,7 +108,7 @@ class Oauth(uvhttp.http.Session):
 
         self.set_token(identifier, token.json())
         return self.get_valid_token(identifier)
-        
+
     async def request(self, *args, identifier=None, **kwargs):
         """
         Make a request, but add the token for the given identifier to
